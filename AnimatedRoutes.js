a119eaa7b5e6a2c941ce4afaefe0c70df7720632
dayanabeirigo <49229699+dayanabeirigo@users.noreply.github.com>
2022-06-03 16:45:25 -0300

    funcionários rotas

---
+++ AnimatedRoutes.js
@@ -0,0 +1,46 @@
+import {useContext} from 'react'
+import { Route, Routes, useLocation,Navigate } from 'react-router-dom'
+import PortalBeneficiario from '../pages/PortalBeneficiario'
+import PortalFuncionario from '../pages/PortalFuncionario'
+import Home from '../pages/Home'
+import SobreNos from '../pages/SobreNos'
+import Parceiros from '../pages/Parceiros'
+import {AnimatePresence} from 'framer-motion'
+import AppRoutes from'../../AppRoutes'
+import CentraldeCestas from '../pages/central de cestas/CentraldeCestas'
+import PaginaPadrao from '../pages/central de cestas/PaginaPadrao'
+import { AuthProvider,AuthContext } from '../../context/Auth'
+import AreadoBeneficiario from '../beneficiarios/AreadoBeneficiario'
+import Funcionario from '../funcionarios/Funcionario'
+
+function AnimatedRoutes() {
+    const location = useLocation();
+    const Private = ({children}) => {
+      const {authenticated,loading} = useContext(AuthContext);
+      if (loading) {
+          return <div className="loading">Carregando...</div>;
+      }
+      if (!authenticated) { 
+          return <Navigate to ="/PortalFuncionario"/>
+       } return children
+  }
+return (
+    <AnimatePresence>
+    <AuthProvider>
+    <Routes location={location} key={location.pathname}>
+    <Route path="/" element={<Home />} />
+    <Route path="/SobreNos" element={<SobreNos/>} />
+    <Route path="/Parceiros" element={<Parceiros/>} />
+    <Route path="/PortalFuncionario" element={<PortalFuncionario />} />
+    <Route path="/PortalBeneficiario" element={<PortalBeneficiario />} />
+    <Route path="/CentraldeCestas" element={<CentraldeCestas/>} />
+    <Route path="/Funcionario" element={<Funcionario/>}/>
+    <Route path="/AreadoBeneficiario" element={<AreadoBeneficiario/>}/>
+  </Routes>
+  </AuthProvider>
+  </AnimatePresence>
+  
+)
+
+}
+export default AnimatedRoutes
